10c670526464902edda24063794577810bfae6e0
Yosrii <zaghouani.yosri@gmail.com>
2021-03-19 12:13:35 +0100

    Update User

--- client/src/Component/Profile.js
+++ client/src/Component/Profile.js
@@ -1,38 +1,183 @@
-import React , {useEffect} from 'react'
-import { useDispatch ,  useSelector } from 'react-redux'
-import { Navbar , Button} from "react-bootstrap"
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Button } from "react-bootstrap"
 import { Redirect } from 'react-router-dom'
-import { getProfile , logout } from '../JS/actions'
-
+import { getProfile, logout } from '../JS/actions'
+import { InputGroup, FormControl } from 'react-bootstrap';
 
 const Profile = () => {
-    const dispatch = useDispatch()
-    const isAuth = useSelector(state => state.userReducer.isAuth)
-    const user = useSelector(state => state.userReducer.user)
-    const loading = useSelector(state => state.userReducer.loading)
+  const dispatch = useDispatch()
+  const isAuth = useSelector(state => state.userReducer.isAuth)
+  const user = useSelector(state => state.userReducer.user)
+  const loading = useSelector(state => state.userReducer.loading)
+  const [edit, setEdit] = useState(false);
 
-    useEffect(() => {
-        dispatch(getProfile());
+  useEffect(() => {
+    dispatch(getProfile());
 
-    }, [dispatch]);
-    
+  }, [dispatch]);
 
-    return loading? (
+
+
+
+  return loading ? (
     <h1>Loading ...</h1>
-   ) : !isAuth? ( 
-    <Redirect to='/login' /> 
-    ) : (
-        <Navbar>
-        <Navbar.Brand href="#home">Mywebsite.com</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end"> 
-          <Navbar.Text >
-            Signed in as: <a href="#login">{user.name}</a>
-          </Navbar.Text>
-          <Button onClick={()=> dispatch(logout())} variant="danger" className="m-2">Logout</Button>
-        </Navbar.Collapse>
-      </Navbar>
-    )
+  ) : !isAuth ? (
+    <Redirect to='/login' />
+  ) : (
+    // <Navbar>
+    //   <Navbar.Brand href="#home">My website.com</Navbar.Brand>
+    //   <Navbar.Toggle />
+    //   <Navbar.Collapse classNameName="justify-content-end">
+    //     <Navbar.Text >
+    //       Signed in as: <a href="#login">{user.name}</a>
+    //     </Navbar.Text>
+    //     <Button onClick={() => dispatch(logout())} variant="danger" classNameName="m-2">Logout</Button>
+    //   </Navbar.Collapse>
+    //  </Navbar >
+    <div className="container emp-profile">
+      <form >
+        <div className="row">
+          <div className="col-md-4">
+            <div className="profile-img">
+              <img src="https://images.unsplash.com/photo-1553867745-6e038d085e86?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydHJhaXRzfGVufDB8MnwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" />
+              <div className="file btn btn-lg btn-primary">
+                Change Photo
+              <input type="file" name="file" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="profile-head">
+              <h5>
+                {user.name}
+              </h5>
+              <h6>
+                {user.email}
+              </h6>
+              <ul className="nav nav-tabs" id="myTab" role="tablist">
+                <li className="nav-item">
+                  <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                </li>
+                {/* <li className="nav-item">
+                  <a className="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+                </li> */}
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <input type="button" className="profile-edit-btn" name="btnAddMore" value="Edit Profile" onClick={() => { setEdit(true) }} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="profile-work">
+              <p>WORK LINK</p>
+              <a href="">Website Link</a><br />
+              <a href="">Bootsnipp Profile</a><br />
+              <a href="">Bootply Profile</a>
+              <p>SKILLS</p>
+              <a href="">Web Designer</a><br />
+              <a href="">Web Developer</a><br />
+              <a href="">WordPress</a><br />
+              <a href="">WooCommerce</a><br />
+              <a href="">PHP, .Net</a><br />
+            </div>
+          </div>
+
+          {
+            edit ?
+
+              <div className="col-md-8">
+                <div className="tab-content profile-tab" id="myTabContent">
+                  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Name</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.name} />
+                        </InputGroup>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Email</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.email} />
+                        </InputGroup>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Phone</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.phoneNumber} />
+                        </InputGroup>
+                      </div>
+                    </div>
+                  </div>
+                  <Button variant="info" style={{ marginLeft: "85%" }} onClick={() => { setEdit(false) }}>Enregistrer</Button>
+
+                </div>
+
+              </div>
+
+              :
+
+
+              <div className="col-md-8">
+                <div className="tab-content profile-tab" id="myTabContent">
+                  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Name</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.name} disabled />
+                        </InputGroup>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Email</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.email} disabled />
+                        </InputGroup>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label>Phone</label>
+                      </div>
+                      <div className="col-md-6">
+                        <InputGroup size="sm" className="mb-3">
+                          <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder={user.phoneNumber} disabled />
+                        </InputGroup>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+          }
+
+
+        </div>
+      </form>
+    </div >
+
+
+
+  )
 }
 
 export default Profile
--- client/src/Component/signup.js
+++ client/src/Component/signup.js
@@ -1,131 +1,122 @@
-
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import register from '../JS/actions/index';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import register from "../JS/actions/index";
 //Bootstrap
 // import "./App.css"
 import "bootstrap/dist/css/bootstrap.css";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
-import { FacebookLoginButton, GoogleLoginButton } from "react-social-login-buttons";
-import { useAlert } from 'react-alert'
+import { FacebookLoginButton, GoogleLoginButton, } from "react-social-login-buttons";
+import { useAlert } from "react-alert";
 // import userReducer from '../JS/reducers/userReducer';
 
-
-
-
 const Signup = () => {
-
-  const loading = useSelector(state => state.userReducer.loading)
-  const user = useSelector(state => state.userReducer.user)
+  const loading = useSelector((state) => state.userReducer.loading);
+  const user = useSelector((state) => state.userReducer.user);
   const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phoneNumber, setPhoneNumber] = useState(null);
+  // const reduxData = useSelector((state) = state.userReducer.errors)
   const alert = useAlert();
 
-  const addUser = e => {
+  // const rm = reduxData.errors[0];
+
+  const addUser = (e) => {
     e.preventDefault();
     dispatch(
       register({
         name,
         email,
         password,
-        phoneNumber
-      }))
-  }
+        phoneNumber,
+      })
+    );
+  };
 
   function handleSubmit(e) {
-    if (!email.includes('@')) {
-      alert.error('invalid email')
+    if (!email && !name && !phoneNumber)
+      alert.error("Tous les champs sont obligatoires");
+    else if (!email.includes("@")) alert.error("invalid email");
+    else if (!name) {
+      alert.error("Le nom est obligatoire");
+    } else if (!phoneNumber) {
+      alert.error("Le numéro de téléphone est obligatoire");
+    } else if (!password) {
+      alert.error("Le mot de passe est obligatoire");
+    } else {
+      addUser(e);
+      alert.success("Welcome to my site !");
     }
-
-    if (!name) {
-      alert.error('invalid name')
-    }
-
-    if (!phoneNumber) {
-      alert.error('invalid phone Number')
-    }
-    else {
-      addUser(e)
-      alert.success('Welcome to my site !')
-    }
-
   }
 
   return (
-    <div >
-      {
-        loading ? (
-          <h1>Attendez  ...</h1>
-        ) : user ? (
-          <Redirect to='/login' />
-        ) : (
+    <div>
+      {loading ? (
+        <h1>Attendez ...</h1>
+      ) : user ? (
+        <Redirect to="/login" />
+      ) : (
+        <Form className="login-form">
+          <h1>
+            <span className="font-weight-bold">mywebsite</span>.com
+          </h1>
 
-          <Form className="login-form">
-            <h1><span className="font-weight-bold">mywebsite</span>.com</h1>
+          <h2 className="text-center">Marhaba ! </h2>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
 
+          <FormGroup>
+            <Label>Nom & prénom</Label>
+            <Input
+              type="name"
+              name="name"
+              placeholder="Nom et prénom"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
 
-            <h2 className="text-center">Marhaba ! </h2>
-            <FormGroup>
-              <Label>Email</Label>
-              <Input
-                type="email"
-                name="email"
-                placeholder="Email"
-                onChange={e => setEmail(e.target.value)
-                }
-              />
-            </FormGroup>
+          <FormGroup>
+            <Label>Tel°</Label>
+            <Input
+              type="text"
+              placeholder="Numéro téléphone"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </FormGroup>
 
-            <FormGroup>
-              <Label>Nom & prénom</Label>
-              <Input
-                type="name"
-                name="name"
-                placeholder="Nom et prénom"
-                onChange={e => setName(e.target.value)}
-              />
+          <FormGroup>
+            <Label>Mot de passe</Label>
+            <Input
+              type="Password"
+              placeholder="Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormGroup>
 
-            </FormGroup>
-
-            <FormGroup>
-              <Label>Tel°</Label>
-              <Input
-                type="text"
-                placeholder="Numéro téléphone"
-                onChange={e => setPhoneNumber(e.target.value)}
-              />
-            </FormGroup>
-
-            <FormGroup>
-              <Label>Mot de passe</Label>
-              <Input
-                type="Password"
-                placeholder="Mot de passe"
-                onChange={e => setPassword(e.target.value)}
-              />
-            </FormGroup>
-
-            <Button className="btn-lg btn-dark btn-block" onClick={handleSubmit}>Valider</Button>
-            <div className="text-center pt-3"> Facebook Ou Google</div>
-            <FacebookLoginButton className="mt-3 mb-3" />
-            <GoogleLoginButton className="mt-3 mb-3" />
-            <div className="text-center">
-              <a href="sign-up">Se Connecter</a>
-              <span className="p-2">|</span>
-              <a href="sign-up">Mot de passe oublié </a>
-            </div>
-          </Form>
-        )}
+          <Button className="btn-lg btn-dark btn-block" onClick={handleSubmit}>
+            Valider
+          </Button>
+          <div className="text-center pt-3"> Facebook Ou Google</div>
+          <FacebookLoginButton className="mt-3 mb-3" />
+          <GoogleLoginButton className="mt-3 mb-3" />
+          <div className="text-center">
+            <a href="sign-up">Se Connecter</a>
+            <span className="p-2">|</span>
+            <a href="sign-up">Mot de passe oublié </a>
+          </div>
+        </Form>
+      )}
     </div>
-
-
-  )
-}
-
-
+  );
+};
 
 export default Signup;
